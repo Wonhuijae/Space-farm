5254f5e8126808eb0cacf304dfeb3023b94959f7
Wonhuijae <whj46262@gmail.com>
2024-09-05 17:48:55 +0900

    2024.09.05 안드로이드 빌드 확인 & UI 사이즈 조정

--- Space Farm/Assets/02. Scripts/FieldCollison.cs
+++ Space Farm/Assets/02. Scripts/FieldCollison.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class FieldCollison : MonoBehaviour
 {
